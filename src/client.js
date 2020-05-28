113191eb035b574fed9ee76eb12bcd23e303582a
lcukerd <harshit05081997@gmail.com>
2020-05-28 19:36:59 +0530

    Choose command from text on random

--- src/client.js
+++ src/client.js
@@ -17,22 +17,20 @@ const encrypt = (data, key) => {
 const msgAck = (msg, priority, result) => {
     console.log(`Message: ${msg} Priority: ${priority} Output: ${result}`);
 }
-
+const randomNumber = length => {
+    return Math.floor(Math.random() * length);
+}
 // Use the stored public key to encrypt messages before sending
 const startMessaging = async () => {
-    let counter = 0;
     let data = fs.readFileSync(path.join(__dirname, '../commands.txt')).toString().split('\n');
     while (true) {
         // Timeout to ensure that client don't hang CPU
         await new Promise((resolve, _reject) => setTimeout(() => resolve('done'), 100));
         let msg = {};
-        msg.message = encrypt(data[counter++], publicKey);
+        msg.message = encrypt(data[randomNumber(data.length)], publicKey);
         msg.priority = Math.floor(Math.random() * 10);
 
         socket.emit('chat', msg, msgAck);
-        if (counter === data.length) {
-            counter = 0;
-        }
     }
 }
 
