23bb79e749ecd2d8054d71aa83782d84ea7179b2
lcukerd <harshit05081997@gmail.com>
2020-05-28 15:53:13 +0530

    Encryption and Ack working fine

--- src/client.js
+++ src/client.js
@@ -1,7 +1,39 @@
-import io from 'socket.io-client';
+const io = require('socket.io-client');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto')
 
-socket = io();
+socket = io.connect('http://localhost:5000');
 
-socket.on('connect', () => {
-    socket.on('register')
-});
+let publicKey;
+
+const encrypt = (data, key) => {
+    const buffer = Buffer.from(data, 'utf8')
+    const encrypted = crypto.publicEncrypt(key, buffer)
+    return encrypted.toString('base64')
+}
+
+const msgAck = (msg, type) => {
+    console.log(`Message: ${msg} was detected of Type: ${type}`);
+}
+
+const startMessaging = async () => {
+    let counter = 0;
+    let data = fs.readFileSync(path.join(__dirname, '../commands.txt')).toString().split('\n');
+    while (true) {
+        await new Promise((resolve, _reject) => setTimeout(() => resolve('done'), 100));
+        const msg = encrypt(data[counter++], publicKey);
+        socket.emit('chat', msg, msgAck);
+        if (counter === data.length) {
+            counter = 0;
+        }
+    }
+}
+
+const receivedKey = key => {
+    publicKey = key;
+    console.log(`Received public key in ${socket.id}`);
+    startMessaging();
+}
+
+socket.emit('register', receivedKey);
--- src/index.js
+++ src/index.js
@@ -1,38 +1,72 @@
 const app = require('http').createServer(handler)
 const io = require('socket.io')(app);
 const fs = require('fs');
-const NodeRSA = require('node-rsa');
+const crypto = require('crypto')
 const port = process.env.PORT || 5000;
 
-app.listen(80);
+app.listen(port);
 
 let mapKey = {};
 
 let msgqueue = [];
 let cmdqueue = [];
 
+const decrypt = (data, key) => {
+    const buffer = Buffer.from(data, 'base64')
+    const decrypted = crypto.privateDecrypt({
+        key: key,
+        passphrase: '',
+    },
+        buffer,
+    )
+    return decrypted.toString('utf8')
+}
+
+function handler(req, res) {
+    res.end('Server running!')
+}
+
+const generateKeys = () => {
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem',
+            cipher: 'aes-256-cbc',
+            passphrase: '',
+        },
+    })
+    return { privateKey, publicKey };
+}
+
 io.on('connection', socket => {
     console.log('Got a new connection');
 
     io.emit('this', { will: 'be recieved by everyone' });
 
-    socket.on('register', (msg, ack) => {
-        const key = msg.key;
+    socket.on('register', ack => {
         console.log(`Registering Client ${socket.id}`);
-        mapKey[socket.id] = key;
+        const key = generateKeys();
+        mapKey[socket.id] = key.privateKey;
+        ack(key.publicKey);
         console.log('Registered')
-        ack();
     });
 
     socket.on('chat', (msg, ack) => {
-        const key = mapKey[socket.id]
-        const message = key.decrypt(msg.message)
+        const key = mapKey[socket.id];
+        const message = decrypt(msg, key);
+
         if (message.charAt(0) === '$') {
             cmdqueue.push(message.replace('$ '));
-            ack('Command');
+            ack(message, 'Command');
         } else {
             msgqueue.push(message);
-            ack('Message');
+            ack(message, 'Message');
         }
+
     });
 })
